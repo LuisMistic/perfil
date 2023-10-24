ea3c1ac695220100a23adeeab4f0c59be2a2bbad
luismistic <luis.escalada@hotmail.com>
2023-10-24 18:03:25 -0300

    animiacion

---
+++ src/app/componentes/matrix/matrix.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-matrix',
+  template: `
+    <div class="matrix-background">
+      <div class="matrix-text">
+        <!-- Aquí puedes agregar tu contenido HTML -->
+      </div>
+    </div>
+  `,
+  styleUrls: ['./matrix.component.css']
+})
+export class MatrixComponent {}
+
